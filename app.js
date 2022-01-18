aa32598ecef2ae7cc91e8e50b82751224ca9d7a2
hungnd2905 <hungnd2905@gmail.com>
2022-01-18 12:09:54 +0100

    add feature: add ToDo List item though a form in frontend, send to backend and show on home route /

--- app.js
+++ app.js
@@ -3,10 +3,37 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 //App.js : server
 //Send message to the browser when user accesses homepage with route "/"
 app.get("/", function(req,res){
-  res.send("Hello");
+  var today = new Date();
+  var currentDay = today.getDay();
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {
+    kindOfDay: day,
+    newListItems: items
+  });
+
+});
+
+app.post("/", function(req, res){
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, function(){
